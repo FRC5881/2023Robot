041a0654f6d4bc1ef30edea2f510abb4e88f7925
Brian Rozmierski <brianr@warped-minds.com>
2023-01-29 17:38:58 -0500

    Added Code Style Linting and CI Testing
    * Updated formatting on existing code to match

--- src/main/java/org/tvhsfrc/frc2023/robot/RobotContainer.java
+++ src/main/java/org/tvhsfrc/frc2023/robot/RobotContainer.java
@@ -13,32 +13,26 @@ import org.tvhsfrc.frc2023.robot.commands.Autos;
 import org.tvhsfrc.frc2023.robot.commands.ExampleCommand;
 import org.tvhsfrc.frc2023.robot.subsystems.ExampleSubsystem;
 
-
-
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
-public class RobotContainer
-{
+public class RobotContainer {
     // The robot's subsystems and commands are defined here...
     private final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
-    
+
     // Replace with CommandPS4Controller or CommandJoystick if needed
     private final CommandXboxController driverController =
             new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
-    
-    
+
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
-    public RobotContainer()
-    {
+    public RobotContainer() {
         // Configure the trigger bindings
         configureBindings();
     }
-    
-    
+
     /**
      * Use this method to define your trigger->command mappings. Triggers can be created via the
      * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
@@ -48,25 +42,22 @@ public class RobotContainer
      * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
      * joysticks}.
      */
-    private void configureBindings()
-    {
+    private void configureBindings() {
         // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
         new Trigger(exampleSubsystem::exampleCondition)
                 .onTrue(new ExampleCommand(exampleSubsystem));
-        
+
         // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
         // cancelling on release.
         driverController.b().whileTrue(exampleSubsystem.exampleMethodCommand());
     }
-    
-    
+
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
      *
      * @return the command to run in autonomous
      */
-    public Command getAutonomousCommand()
-    {
+    public Command getAutonomousCommand() {
         // An example command will be run in autonomous
         return Autos.exampleAuto(exampleSubsystem);
     }
--- src/main/java/org/tvhsfrc/frc2023/robot/commands/Autos.java
+++ src/main/java/org/tvhsfrc/frc2023/robot/commands/Autos.java
@@ -9,19 +9,13 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import org.tvhsfrc.frc2023.robot.subsystems.ExampleSubsystem;
 
-
-
-public final class Autos
-{
+public final class Autos {
     /** Example static factory for an autonomous command. */
-    public static CommandBase exampleAuto(ExampleSubsystem subsystem)
-    {
+    public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
         return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
     }
-    
-    
-    private Autos()
-    {
+
+    private Autos() {
         throw new UnsupportedOperationException("This is a utility class!");
     }
 }
