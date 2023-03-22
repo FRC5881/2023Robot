255fc957f1cd9e49ea77a5de57962d725accbd69
Christopher Mahoney <56850185+Alextopher@users.noreply.github.com>
2023-03-22 15:28:22 -0400

    Update VacuumToggleCommand.java

--- src/main/java/org/tvhsfrc/frc2023/robot/commands/vacuum/VacuumToggleCommand.java
+++ src/main/java/org/tvhsfrc/frc2023/robot/commands/vacuum/VacuumToggleCommand.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import org.tvhsfrc.frc2023.robot.Constants;
 import org.tvhsfrc.frc2023.robot.subsystems.VacuumSubsystem;
 
-// Toggles the vacuum pump on and off
+// Toggles the vacuum subsystem on and off
 public class VacuumToggleCommand extends ConditionalCommand {
     public VacuumToggleCommand(VacuumSubsystem vacuumSubsystem) {
         super(
