36362f5837919b05522056827121dc9f77b9a48a
Brian Rozmierski <brianr@warped-minds.com>
2023-03-22 20:01:01 -0400

    Vacuum Update
    . Refactored `VacuumSubsystem` to use Enum `state`
    . Timeout for switchable dump state now in `periodic()`
    . Removed closed loop control
    . Simplified to single (toggle type) command

--- src/main/java/org/tvhsfrc/frc2023/robot/subsystems/VacuumSubsystem.java
+++ src/main/java/org/tvhsfrc/frc2023/robot/subsystems/VacuumSubsystem.java
@@ -4,18 +4,21 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.simulation.SimDeviceSim;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.tvhsfrc.frc2023.robot.Constants;
 import org.tvhsfrc.frc2023.robot.Robot;
-import org.tvhsfrc.frc2023.robot.commands.VacuumDisableCommand;
-import org.tvhsfrc.frc2023.robot.commands.VacuumEnableCommand;
+import org.tvhsfrc.frc2023.robot.commands.VacuumCommand;
 
 public class VacuumSubsystem extends SubsystemBase {
+    public enum State {
+        VACUUM,
+        DUMP
+    }
+
     private final PowerDistribution pdh;
     private final SimDeviceSim pdhSim = new SimDeviceSim("Power Distribution Switch");
 
@@ -31,19 +34,23 @@ public class VacuumSubsystem extends SubsystemBase {
 
     private final SimDeviceSim motorsSim = new SimDeviceSim("Vacuum Motors");
 
+    private final Timer dumpTimer = new Timer();
+
     /** state of the subsystem: true if we're running the vacuum, false if we're not */
-    private boolean state = false;
+    private State state = State.DUMP;
 
     public VacuumSubsystem(PowerDistribution pdh) {
         this.pdh = pdh;
 
-        vacuum1.getPIDController().setP(0.00005);
+        /*vacuum1.getPIDController().setP(0.00005);
         vacuum1.getPIDController().setI(0.0);
         vacuum1.getPIDController().setD(0.002);
         vacuum1.getPIDController().setFF(0.0001);
         vacuum1.setIdleMode(CANSparkMax.IdleMode.kCoast);
         vacuum1.getPIDController().setOutputRange(0, Constants.Vacuum.MAX_OUTPUT);
-        vacuum1.setClosedLoopRampRate(0.5);
+        vacuum1.setClosedLoopRampRate(0.5);*/
+
+        vacuum1.setIdleMode(CANSparkMax.IdleMode.kCoast);
 
         pdh.setSwitchableChannel(false);
 
@@ -52,21 +59,19 @@ public class VacuumSubsystem extends SubsystemBase {
 
         ShuffleboardTab tab = Shuffleboard.getTab("Vacuum");
 
-        tab.add("cToggle", cToggle());
-        tab.add(new VacuumEnableCommand(this));
-        tab.add(new VacuumDisableCommand(this, Constants.Vacuum.DUMP_TIME));
+        tab.add(new VacuumCommand(this));
 
         tab.add(this);
     }
 
     @Override
     public void initSendable(SendableBuilder builder) {
-        builder.addBooleanProperty("Vacuum state", () -> state, null);
+        builder.addStringProperty("Vacuum state", () -> state.toString(), null);
 
         if (Robot.isReal()) {
             builder.addBooleanProperty(
-                    "PDH switchable channel", () -> pdh.getSwitchableChannel(), null);
-            builder.addDoubleProperty("Applied Output", () -> vacuum1.getAppliedOutput(), null);
+                    "PDH switchable channel", pdh::getSwitchableChannel, null);
+            builder.addDoubleProperty("Applied Output", vacuum1::getAppliedOutput, null);
         } else {
             builder.addBooleanProperty(
                     "PDH switchable channel", () -> pdhSim.getBoolean("Value").get(), null);
@@ -75,70 +80,67 @@ public class VacuumSubsystem extends SubsystemBase {
         }
     }
 
-    /**
-     * Run or stop the vacuum motors.
-     *
-     * @param shouldSuck
-     */
-    public void suck(boolean shouldSuck) {
-        if (Robot.isReal()) {
-            if (shouldSuck) {
-                vacuum1.getPIDController()
-                        .setReference(
-                                Constants.Vacuum.VACUUM_VELOCITY,
-                                CANSparkMax.ControlType.kVelocity);
-            } else {
-                vacuum1.stopMotor();
-            }
-        } else {
-            motorsSim.getDouble("Applied Output").set(shouldSuck ? 1 : 0);
+    @Override
+    public void periodic() {
+        if (state.equals(State.DUMP)
+                && dumpTimer.hasElapsed(Constants.Vacuum.DUMP_TIME)
+                && pdh.getSwitchableChannel()) {
+            pdh.setSwitchableChannel(false);
         }
     }
 
-    /**
-     * Opens or closes the dump valve.
-     *
-     * @param shouldDump true to open the valve, false to close it
-     */
-    public void dump(boolean shouldDump) {
+    @Override
+    public void simulationPeriodic() {
+        if (state.equals(State.DUMP)
+                && dumpTimer.hasElapsed(Constants.Vacuum.DUMP_TIME)
+                && pdh.getSwitchableChannel()) {
+            pdhSim.getBoolean("Value").set(false);
+        }
+    }
+
+    /** Run the vacuum motors. */
+    public void vacuum() {
+        setState(State.VACUUM);
         if (Robot.isReal()) {
-            pdh.setSwitchableChannel(shouldDump);
+            pdh.setSwitchableChannel(false);
+            vacuum1.set(Constants.Vacuum.MAX_OUTPUT);
         } else {
-            pdhSim.getBoolean("Value").set(shouldDump);
+            pdhSim.getBoolean("Value").set(false);
+            motorsSim.getDouble("Applied Output").set(1);
+        }
+    }
+
+    /** Opens the dump valve and stops the motors. */
+    public void dump() {
+        setState(State.DUMP);
+        if (Robot.isReal()) {
+            vacuum1.stopMotor();
+            pdh.setSwitchableChannel(true);
+            dumpTimer.restart();
+        } else {
+            pdhSim.getBoolean("Value").set(true);
+            motorsSim.getDouble("Applied Output").set(0);
         }
     }
 
     /**
      * Set the state of the vacuum subsystem.
      *
-     * <p>This method is only used to record state changes. Calling this fuction will not cause the
+     * <p>This method is only used to record state changes. Calling this function will not cause the
      * vacuum to start or stop.
      *
-     * @param state true to enable, false to disable
+     * @param state State enum of the subsystem
      */
-    public void setState(boolean state) {
+    private void setState(State state) {
         this.state = state;
     }
 
     /**
      * Get the state of the vacuum subsystem.
      *
-     * @return true if the vacuum is enabled, false if it is disabled
+     * @return State enum
      */
-    public boolean getState() {
+    public State getState() {
         return state;
-    }
-
-    /**
-     * Creates a command that toggles the vacuum subsystem.
-     *
-     * @see VacuumEnableCommand
-     * @see VacuumDisableCommand
-     */
-    public CommandBase cToggle() {
-        return Commands.either(
-                new VacuumDisableCommand(this, Constants.Vacuum.DUMP_TIME),
-                new VacuumEnableCommand(this),
-                () -> state);
     }
 }
